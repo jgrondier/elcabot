afd428baff60002c46a776d6d777be75a7bd0444
Boris Goullet <boris.goullet@gmail.com>
2018-10-12 09:04:47 +0200

    added bot token

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ def error(bot, update, error):
 def main():
     """Start the bot."""
     # Create the EventHandler and pass it your bot's token.
-    updater = Updater("TOKEN")
+    updater = Updater("634016753:AAEyVHAXD6pL9enfmS-wudvZt7EQTB2ReS4")
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
