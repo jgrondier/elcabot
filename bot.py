767502e914e4b7dbc8f5b8b85365b6849b9fec7f
jgrondier <juliengrondier@gmail.com>
2018-10-12 11:57:40 +0200

    Presentation version

--- bot.py
+++ bot.py
@@ -3,11 +3,12 @@
 
 """Simple Bot to reply to Telegram messages.
 """
-
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackQueryHandler
 import logging
 from intent_processing import get_location_recommendation
 from yelp import search_yelp
+import geopy.distance
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -25,6 +26,10 @@ locations_dict = {
 }
 
 btypes_dict = {
+
+}
+
+results_dict = {
 
 }
 
@@ -81,10 +86,10 @@ def business_choice(bot, update):
     btypes_dict[user.id] = btype
 
     update.message.reply_text(
-        "Great, you're looking for a {}. Let's look at what we have near you.".format(btype))
+        "Great, you're looking for a {}. Let's look at what we have near you.".format(btype[0]))
 
     logger.info(
-        "Received business type intent {} from user {}".format(btype, user.id))
+        "Received business type intent {} from user {}".format(btype[0], user.id))
 
     user = update.message.from_user
 
@@ -92,18 +97,48 @@ def business_choice(bot, update):
 
     result = search_yelp(business_type=btypes_dict[user.id], location=loc)
 
-    update.message.reply_text("Ok, so here is what I found: ")
+    results_dict[user.id] = result
+
+    l1 = (loc["latitude"], loc["longitude"])
+
+    update.message.reply_text("Ok, so here is what I found: ",
+                              reply_markup=InlineKeyboardMarkup([
+                                  [InlineKeyboardButton("{}, ({})".format(
+                                      x["name"],
+                                      geopy.distance.distance(
+                                          l1, (x["location"][0], x["location"][1]))
+                                  ), callback_data=x["name"])] for x in result
+                              ]))
 
     logger.info(result)
 
-    update.message.reply_venue(
-        latitude=result["location"][0],
-        longitude=result["location"][1],
-        title=result["name"],
-        address=result["display_address"]
-    )
+    return ConversationHandler.END
+
+
+def menu_actions(bot, update):
+
+    logger.info(update)
+
+    user = update.callback_query.message.chat.id
+    query = update.callback_query
+
+    for result in results_dict[user]:
+        if result["name"] == query.data:
+            bot.send_venue(user,
+                           latitude=result["location"][0],
+                           longitude=result["location"][1],
+                           title=result["name"],
+                           address=result["display_address"]
+                           )
+
+    bot.send_message(user, "Thank you for trying our bot !")
 
     return ConversationHandler.END
+
+
+def result(bot, update):
+
+    pass
 
 
 def cancel(bot, update):
@@ -121,7 +156,7 @@ def error(bot, update, error):
 def main():
     """Start the bot."""
     # Create the EventHandler and pass it your bot's token.
-    updater = Updater("634016753:AAEyVHAXD6pL9enfmS-wudvZt7EQTB2ReS4")
+    updater = Updater("API TOKEN")
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
@@ -133,6 +168,7 @@ def main():
         states={
             LOCATION: [MessageHandler(Filters.text | Filters.location, location)],
             BUSINESS_CHOICE: [MessageHandler(Filters.text, business_choice)],
+            RESULT: [MessageHandler(Filters.text, menu_actions)]
         },
 
         fallbacks=[CommandHandler('cancel', cancel)]
@@ -141,6 +177,8 @@ def main():
 
     # on different commands - answer in Telegram
     dp.add_handler(conv_handler)
+
+    dp.add_handler(CallbackQueryHandler(menu_actions))
 
     # log all errors
     dp.add_error_handler(error)
--- yelp.py
+++ yelp.py
@@ -2,7 +2,7 @@ from yelpapi import YelpAPI
 #import pprint
 
 def search_yelp(business_type, location, limit = 5):
-    yelp_api = YelpAPI('mycs0Rr2L8hy4QUL_cvA5Kyv2i7iCqgFR84V29ZW1TqIsWO7MQjunW8deES8-1g6hDuFfout3x-r1706Xi_qSjUutX3olWO-RlCqghBselLBu9J5v7x6YOEBXFzAW3Yx')
+    yelp_api = YelpAPI('YELP TOKEN')
     search_results = yelp_api.search_query(term=business_type, location=str(location['latitude'])+","+str(location['longitude']))
     results = search_results['businesses'][:limit]
     return [{'location':(result['coordinates']['latitude'], result['coordinates']['longitude']), 'name':result['name'], 'url': result['url'], 'display_address': ', '.join(result['location']['display_address'])} for result in results]  
